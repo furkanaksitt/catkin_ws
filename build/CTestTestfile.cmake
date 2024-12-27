c6c62b01dcc5d84ee38cfd0ec0095241e0418c94
eng401 <furkanaksitt@gmail.com>
2024-12-27 21:54:02 +0300

    İlk versiyon gazebo moveit birlikte çalışmıyor. Display dosyası aktif

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/eng401/catkin_ws/src
+# Build directory: /home/eng401/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("mover")
+subdirs("my_spherical_manipulator")
---
+++ build/mover/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/eng401/catkin_ws/src/mover
+# Build directory: /home/eng401/catkin_ws/build/mover
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
